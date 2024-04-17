1455221e07dd8c640e5ed75dc6fa3f1e54ee61b5
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-17 15:31:44 +0800

    feat: made wget display progress bar

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.
 sed -i '/plugins=(/,/)/c\plugins=(\n  git\n  zsh-completions\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n)' $ZSHRC_PATH
 
 # Powerlevel10k Config
-wget https://raw.githubusercontent.com/OnCloud125252/Oh-My-Zsh-Setup/main/.p10k.zsh -O $HOME/.p10k.zsh
-wget https://raw.githubusercontent.com/OnCloud125252/Oh-My-Zsh-Setup/main/.p10k-warp.zsh -O $HOME/.p10k-warp.zsh
+wget https://raw.githubusercontent.com/OnCloud125252/Oh-My-Zsh-Setup/main/.p10k.zsh -q --show-progress --progress=bar:force -O $HOME/.p10k.zsh
+wget https://raw.githubusercontent.com/OnCloud125252/Oh-My-Zsh-Setup/main/.p10k-warp.zsh -q --show-progress --progress=bar:force -O $HOME/.p10k-warp.zsh
 sed -i 's/\[\[ ! -f ~\/.p10k\.zsh \]\] || source ~\/.p10k\.zsh/if \[\[ $(printenv | grep WARP) \]\];\nthen\n\t[[ ! -f ~\/.p10k-warp.zsh \]\] || source ~\/.p10k-warp.zsh\nelse\n\t[[ ! -f ~\/.p10k.zsh \]\] || source ~\/.p10k.zsh\nfi/' $ZSHRC_PATH
 
 source $ZSHRC_PATH
